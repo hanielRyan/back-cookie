8195ffabf8db71930585135a3bb141797ebda05a
Haniel Ryan <143314778+hanielRyan@users.noreply.github.com>
2023-12-24 00:58:08 +0400

    Update index.js 6

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const parser=require("cookie-parser");
 const app=express();
 const cors=require("cors");
 app.use(cors({
-    origin:"https://front-cookie.vercel.app",
+    origin:"http://localhost:3000",
     credentials:true
 }));
+app.use(express.json());
 app.use(parser());
-app.post("/",(req,res)=>{
-    res.set("Access-Control-Allow-Origin","https://front-cookie.vercel.app");
-    res.set("Access-Control-Allow-credentials","true");
-    res.cookie("name",req.body.name,{secure:true,sameSite:"none"});
-    res.status(200).json("sent cookie");
+app.post("/post",(req,res)=>{
+    res.set("Access-Control-Allow-Origin","http://localhost:3000");
+    res.set("Access-Control-Allow-Credentials","true");
+    res.cookie("name",req.body.name,{httpOnly:true,sameSite:"strict"});
+    res.json(`sent cookie ${req.body.name}`);
 })
 app.listen(5000,()=>console.log("port created"))
